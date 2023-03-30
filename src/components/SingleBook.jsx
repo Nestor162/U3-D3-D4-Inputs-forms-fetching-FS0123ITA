a659c1b1e51abb85df24e2f05fd056f05c08077f
Nestor162 <nestorcicardini@gmail.com>
2023-03-30 19:28:17 +0200

    Funzione seleziona libri

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,30 +1,47 @@
+import { Component } from "react";
 import { Card, Badge, Button } from "react-bootstrap";
 
-function SingleBook(props) {
-  return (
-    <Card className="h-100">
-      <Card.Img className="img-fluid" variant="top" src={props.book.img} />
-      <Card.Body className="d-flex flex-column justify-content-around">
-        <Card.Title className="text-truncate">{props.book.title}</Card.Title>
-        <h5>
-          Prezzo:
-          <Badge bg="success" className="ms-2">
-            {props.book.price}€
-          </Badge>
-        </h5>
-        <h5>
-          Categoria:
-          <Badge bg="secondary" className="ms-2">
-            {props.book.category}
-          </Badge>
-        </h5>
+class SingleBook extends Component {
+  state = {
+    selected: false
+  };
 
-        <Button variant="primary" className="w-100">
-          Scopri di più...
-        </Button>
-      </Card.Body>
-    </Card>
-  );
+  handleOnClick = () => {
+    this.setState({ selected: !this.state.selected });
+  };
+  render() {
+    return (
+      <Card className={`h-100 ${this.state.selected ? "selected" : ""}`}>
+        <Card.Img
+          className="img-fluid"
+          variant="top"
+          src={this.props.book.img}
+          onClick={() => {
+            this.setState(this.handleOnClick);
+          }}
+        />
+        <Card.Body className="d-flex flex-column justify-content-around">
+          <Card.Title className="text-truncate">{this.props.book.title}</Card.Title>
+          <h5>
+            Prezzo:
+            <Badge bg="success" className="ms-2">
+              {this.props.book.price}€
+            </Badge>
+          </h5>
+          <h5>
+            Categoria:
+            <Badge bg="secondary" className="ms-2">
+              {this.props.book.category}
+            </Badge>
+          </h5>
+
+          <Button variant="primary" className="w-100">
+            Scopri di più...
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default SingleBook;
